1f1b5fe884285767ba110bab29f96517378a18fb
Matías Bellone <matias.bellone@readyset.co>
2025-05-20 14:41:41 +1000

    Updated bintest package to v3

--- pipeline_test.go
+++ pipeline_test.go
@@ -5,7 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/buildkite/bintest"
+	"github.com/buildkite/bintest/v3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
